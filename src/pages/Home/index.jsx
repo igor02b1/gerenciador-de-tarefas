b9edaabad492006fc18da64a753a3f4822fb5479
Igor02b1 <igor.lsilva@al.infnet.edu.br>
2025-01-03 21:31:30 -0300

    estilizando container de registrar tarefa

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,30 @@
+import styles from './styles.module.css';
+
 export default function Home() {
     return (
-        <div>
-            <h1>hello world</h1>  
+        <div className={styles.container}>      
+            <div className={styles.container_form}>
+                <form className={styles.form} onSubmit={(event) => console.log("teste")} >
+                    <h2>Nova tarefa</h2>
+
+                    <input placeholder="Titúlo"/>
+                    
+                    <select>
+                        <option>trabalhar</option>
+                        <option>estudar</option>
+                    </select>      
+
+                    <input type="date" placeholder="data"/>
+
+                    <input placeholder="descrição"/>
+
+                    <button type="submit">salvar</button>
+                </form>
+            </div>
+
+            <div className="container-list">
+                <h3>lista de tarefas</h3>
+            </div>
         </div>
     )
 }
